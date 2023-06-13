0a0fcdde6c0d0f53335840622f22355e864e9d0b
Jan Kędzierski <jkedzierski087@gmail.com>
2023-06-13 22:01:40 +0200

    add tickets left endpoint

--- client/src/components/features/Concert/Concert.js
+++ client/src/components/features/Concert/Concert.js
@@ -1,30 +1,8 @@
 import { Row, Col } from 'reactstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { getSeats, loadSeatsRequest, loadSeats } from '../../../redux/seatsRedux';
-import { useEffect, useState } from 'react';
-import io from 'socket.io-client';
 
 import './Concert.scss';
 
-const Concert = ({ performer, price, genre, day, image, chosenDay }) => {
-  const dispatch = useDispatch();
-  const seats = useSelector(getSeats);
-
-  const [socket, setSocket] = useState(null);
-
-  useEffect(() => {
-    const newSocket = io(process.env.PORT || 'http://localhost:8000/');
-    dispatch(loadSeatsRequest());
-    newSocket.on('seatsUpdated', (updatedSeats) => dispatch(loadSeats(updatedSeats)));
-    setSocket(newSocket);
-    return () => {
-      newSocket.close();
-    };
-  }, [dispatch]);
-
-  const takenSeatsCount = seats.filter((item) => item.day === chosenDay).length;
-  const totalSeats = 50 - takenSeatsCount;
-
+const Concert = ({ performer, price, genre, day, image, tickets }) => {
   return (
     <article className="concert">
       <Row noGutters>
@@ -38,7 +16,7 @@ const Concert = ({ performer, price, genre, day, image, chosenDay }) => {
             <img className="concert__info__back" src={image} alt={performer} />
             <h2 className="concert__info__performer">{performer}</h2>
             <h3 className="concert__info__genre">{genre}</h3>
-            <p className="concert__info__tickets">Only {totalSeats} tickets left!</p>
+            <p className="concert__info__tickets">Only {tickets} tickets left!</p>
             <p className="concert__info__day-n-price">
               Day: {day}, Price: {price}$
             </p>
--- controllers/concerts.controller.js
+++ controllers/concerts.controller.js
@@ -1,10 +1,20 @@
-const Concert = require('../models/concert.model')
+const Concert = require('../models/concert.model');
+const Seat = require('../models/seat.model');
 
 exports.getAll = async (req, res) => {
   try {
-    res.json(await Concert.find({}));
-  }
-  catch(err) {
+    const concerts = await Concert.find({});
+    const seats = await Seat.find();
+
+    const updatedConcerts = concerts.map((concert) => {
+      const concertObj = concert.toObject();
+      concertObj.tickets = 50 - seats.filter((seat) => seat.day === concertObj.day).length;
+
+      return concertObj;
+    });
+
+    res.json(updatedConcerts);
+  } catch (err) {
     res.status(500).json({ message: err });
   }
 };
@@ -14,10 +24,9 @@ exports.getRandom = async (req, res) => {
     const count = await Concert.countDocuments();
     const rand = Math.floor(Math.random() * count);
     const con = await Concert.findOne().skip(rand);
-    if(!con) res.status(404).json({ message: 'Not found' });
+    if (!con) res.status(404).json({ message: 'Not found' });
     else res.json(con);
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).json({ message: err });
   }
 };
@@ -25,56 +34,46 @@ exports.getRandom = async (req, res) => {
 exports.getById = async (req, res) => {
   try {
     const con = await Concert.findById(req.params.id);
-    if(!con) res.status(404).json({ message: 'Not found' });
+    if (!con) res.status(404).json({ message: 'Not found' });
     else res.json(con);
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).json({ message: err });
+  }
+};
 
-  };
-}
-
-exports.post = async(req, res) => {
+exports.post = async (req, res) => {
   try {
-
     const { performer, genre, price, day, image } = req.body;
-    const newConcert = new Concert({ performer: performer, genre: genre, price: price, day: day, image: image  });
+    const newConcert = new Concert({ performer: performer, genre: genre, price: price, day: day, image: image });
     await newConcert.save();
     res.json({ message: 'OK' });
-
-  } catch(err) {
+  } catch (err) {
     res.status(500).json({ message: err });
   }
 };
 
 exports.update = async (req, res) => {
-
   const { performer, genre, price, day, image } = req.body;
 
   try {
     const con = await Concert.findById(req.params.id);
-    if(con) {
-      await Concert.updateOne({ _id: req.params.id }, { $set: { performer: performer, genre: genre, price: price, day: day, image: image }});
+    if (con) {
+      await Concert.updateOne({ _id: req.params.id }, { $set: { performer: performer, genre: genre, price: price, day: day, image: image } });
       res.json({ message: 'Document edited', con });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch(err) {
+    } else res.status(404).json({ message: 'Not found...' });
+  } catch (err) {
     res.status(500).json({ message: err });
   }
 };
 
 exports.delete = async (req, res) => {
-
   try {
     const con = await Concert.findById(req.params.id);
-    if(con) {
+    if (con) {
       await Concert.deleteOne({ _id: req.params.id });
       res.json({ message: 'Deleted', con });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch(err) {
+    } else res.status(404).json({ message: 'Not found...' });
+  } catch (err) {
     res.status(500).json({ message: err });
   }
-}
+};
